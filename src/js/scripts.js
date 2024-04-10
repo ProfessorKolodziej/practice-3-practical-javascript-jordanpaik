f4035c13854c94dae738be79fd051785d290007f
jordanpaik <jspaik@bu.edu>
2024-04-10 19:21:47 -0400

    practice

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,3 +5,41 @@
 // - Do not use onclick - use addEventListener instead
 // - Run npm run test regularly to check autograding
 // - You'll need to link this file to your HTML :)
+
+console.log("hello");
+
+const myName = "jordan";
+
+console.log(myName);
+
+let myAge = 20;
+
+myAge += 1;
+
+console.log(myAge);
+
+
+/* multi
+line 
+comment
+*/
+
+// Practical JavaScript
+
+// Find Element to Hide
+const toggledParagraph = document.querySelector('.show'); // query selector finds the first element on the page that matches selector and then stops
+console.log(toggledParagraph);
+
+// write a function that toggles the show/hide
+function toggleParagraph() {
+    toggledParagraph.classList.toggle("hide");
+    toggledParagraph.classList.toggle("show");
+    console.log("My toggle function");
+    console.log(toggledParagraph);
+}
+
+// find button
+
+const toggleButton = document.querySelector("#button");
+
+toggleButton.addEventListener("click", toggleParagraph);
